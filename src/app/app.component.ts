f3c8c23798af8a0beb9b0337383152a7b732c5e7
Alejandro López <mainusone1@gmail.com>
2023-06-04 01:42:28 +0200

    Major update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+  front: string[] = ["HTML5", "CSS3", "SCSS/Sass", "Bootstrap", "Tailwind"];
+  front2: string[] = ["JavaScript", "jQuery", "TypeScript", "Angular", "Vue"];
+  back: string[] = ["Java", "Python", "Django", "C#", ".NET", "ASP.NET Core", "PHP"];
+  db: string[] = ["MySQL", "Oracle", "SQLite", "PL/SQL", "MongoDB"];
+  mobiles: string[] = ["IONIC"];
+  other: string[] = ["Git", "GitHub", "Docker", "Behat"];
+  design: string[] = ["Wordpress", "Drupal", "Mockflow"];
 }
